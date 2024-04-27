067a1e947ac960a4bee064f75c819cebbfb03526
matishdiggikar <130670389+matishdiggikar@users.noreply.github.com>
2024-04-28 02:40:15 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,9 +28,9 @@ ingredients_list=st.multiselect(
 )
 
 if ingredients_list:
-        ingredients_string=''
+     ingredients_string=''
 
-        for fruit_chosen in ingredients_list:
+    for fruit_chosen in ingredients_list:
                 ingredients_string+=fruit_chosen+' '
                 st.subheader(fruit_chosen+'Nutrition Information:')
                 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_chosen)
@@ -39,7 +39,7 @@ if ingredients_list:
         #st.write(ingredients_string)
        
     
-        my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
+    my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
                 values ('""" + ingredients_string + """','""" + name_on_order + """')"""
 
     #st.write(my_insert_stmt)
