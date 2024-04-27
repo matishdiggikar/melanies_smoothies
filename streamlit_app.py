4a76aa27a852747be103c7a61f0c3a0ef670620a
matishdiggikar <130670389+matishdiggikar@users.noreply.github.com>
2024-04-28 02:02:12 +0530

    Update streamlit_app.py
    SEARCH_ON

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,8 +16,9 @@ st.write('The Name On Your Smoothe Will Be:', name_on_order)
 cnx=st.connection("snowflake")
 session=cnx.session()
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop
 
 ingredients_list=st.multiselect(
     'Choose up to 5 ingredients:'
@@ -26,13 +27,13 @@ ingredients_list=st.multiselect(
 )
 
 if ingredients_list:
-    ingredients_string=''
+   ingredients_string=''
 
-    for fruit_chosen in ingredients_list:
-            ingredients_string+=fruit_chosen+' '
-            st.subheader(fruit_chosen+'Nutrition Information:')
-            fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_chosen)
-            fv_df=st.dataframe(data=fruityvice_response.json(),use_container_width=True)
+        for fruit_chosen in ingredients_list:
+                ingredients_string+=fruit_chosen+' '
+                st.subheader(fruit_chosen+'Nutrition Information:')
+                fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_chosen)
+                fv_df=st.dataframe(data=fruityvice_response.json(),use_container_width=True)
     #st.write(ingredients_string)
 
     
