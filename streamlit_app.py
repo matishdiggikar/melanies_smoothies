d7a957bec12947ab1c6c7087530999c46657b723
matishdiggikar <130670389+matishdiggikar@users.noreply.github.com>
2024-04-27 22:55:04 +0530

    Update streamlit_app.py
    Connected changes needed..... added like 14 and changed 15

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col 
 
 
@@ -9,11 +8,12 @@ st.title("SMOOTHIES!:mango:")
 st.write(
     """SELECT YOUR SMOOTHIES! """)
 
-
 name_on_order = st.text_input('Name On Smoothies:')
 st.write('The Name On Your Smoothe Will Be:', name_on_order)
 
-session = get_active_session()
+cnx=st.connection("snowflake")
+session=cnx.session()
+
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
