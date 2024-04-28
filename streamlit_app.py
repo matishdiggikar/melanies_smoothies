c0ac4e27a61f841726b3a9b92abcfcd20a057203
matishdiggikar <130670389+matishdiggikar@users.noreply.github.com>
2024-04-28 11:36:48 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,12 +26,12 @@ ingredients_list=st.multiselect(
      )
 
 if ingredients_list:
-        ingredients_string=''
+    ingredients_string=''
 
-        for fruit_chosen in ingredients_list:
-                ingredients_string+=fruit_chosen+' '
-                fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-                fv_df=st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+    for fruit_chosen in ingredients_list:
+            ingredients_string+=fruit_chosen+' '
+            fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+            fv_df=st.dataframe(data=fruityvice_response.json(), use_container_width=True)
    
     #st.write(ingredients_string)
 
